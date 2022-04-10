eb9b949b76cb2c2e9cd630c5a084712e8aebc77b
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-10 16:13:49 +0000

    deploy: 59d6cb249547b960cdb03688c3589d2286edfa4d

--- _nuxt/static/1649599520/detail/政治大學統計營/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649599520",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E6%94%BF%E6%B2%BB%E5%A4%A7%E5%AD%B8%E7%B5%B1%E8%A8%88%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- _nuxt/static/1649599520/detail/臺灣師範大學心輔營/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649599520",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E8%87%BA%E7%81%A3%E5%B8%AB%E7%AF%84%E5%A4%A7%E5%AD%B8%E5%BF%83%E8%BC%94%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
