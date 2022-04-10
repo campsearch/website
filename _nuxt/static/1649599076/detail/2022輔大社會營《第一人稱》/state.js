f8ce22509250a2db1832ba6d12ef09a1d90b777e
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-10 14:05:42 +0000

    deploy: ef460504455d68bd0f6f38005d00ed52c0c9bf5b

--- _nuxt/static/1649599076/detail/2022輔大社會營《第一人稱》/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649599076",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F2022%E8%BC%94%E5%A4%A7%E7%A4%BE%E6%9C%83%E7%87%9F%E3%80%8A%E7%AC%AC%E4%B8%80%E4%BA%BA%E7%A8%B1%E3%80%8B",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- _nuxt/static/1649599076/detail/陽明交通大學生技檢驗體驗營/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649599076",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E9%99%BD%E6%98%8E%E4%BA%A4%E9%80%9A%E5%A4%A7%E5%AD%B8%E7%94%9F%E6%8A%80%E6%AA%A2%E9%A9%97%E9%AB%94%E9%A9%97%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
