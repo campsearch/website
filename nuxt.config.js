acdb4a27664150bf51450050b97f3fab38057a2a
Your Name <you@example.com>
2022-04-10 15:56:38 +0800

    完成搜尋系統

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 import colors from 'vuetify/es5/util/colors'
+import zhHant from './lang/zh-Hant'
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
@@ -9,23 +10,21 @@ export default {
     titleTemplate: '%s - camp-search-nuxt',
     title: 'camp-search-nuxt',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
+      {name: 'format-detection', content: 'telephone=no'}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -61,7 +60,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -73,10 +72,15 @@ export default {
           success: colors.green.accent3
         }
       }
+    },
+    lang: {
+      locales: {
+        zhHant
+      },
+      current: 'zhHant'
     }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {}
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,137 +1,67 @@
 <template>
   <v-container>
-    <v-row justify="center" align="center">
-      <v-col cols="12" sm="8" md="6">
-        <v-card class="logo py-4 d-flex justify-center">
-          <NuxtLogo/>
-          <VuetifyLogo/>
-        </v-card>
-        <v-card>
-          <v-card-title class="headline">
-            Welcome to the Vuetify + Nuxt.js template
-          </v-card-title>
-          <v-card-text>
-            <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower
-              developers to create amazing applications.</p>
-            <p>
-              For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-            </p>
-            <p>
-              If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-            </p>
-            <p>
-              Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-            </p>
-            <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the
-              future.</p>
-            <div class="text-xs-right">
-              <em><small>&mdash; John Leider</small></em>
-            </div>
-            <hr class="my-3">
-            <a
-              href="https://nuxtjs.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Nuxt Documentation
-            </a>
-            <br>
-            <a
-              href="https://github.com/nuxt/nuxt.js"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Nuxt GitHub
-            </a>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer/>
-            <v-btn
-              color="primary"
-              nuxt
-              to="/inspire"
-            >
-              Continue
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
     <v-row>
-      <v-col cols="12">
-        <v-card class="px-4 pb-5">
-          <v-card-title class="font-bold px-0">
-            篩選條件
+      <v-col cols="12" style="min-height: calc(50vh);">
+        <v-card>
+          <v-card-title>
+            111暑假營隊
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="搜尋"
+              single-line
+              hide-details
+            ></v-text-field>
           </v-card-title>
-          <v-text-field label="搜尋" prepend-inner-icon="mdi-magnify" v-model="searchContent" class="mb-2"
-                        hide-details></v-text-field>
+
           <v-autocomplete
             v-model="tags"
             :items="categories"
-            dense
             chips
             small-chips
             label="篩選標籤"
             multiple
             solo
             hide-details
-            class="border-b-2"
+            class="px-4 mb-3"
           ></v-autocomplete>
-          <v-select
-            v-model="resultCountPerPage"
-            :items="pageSelect"
-            label="每頁顯示數量"
-            required
-            hide-details
-          ></v-select>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" style="min-height: calc(50vh);">
-        <v-alert type="warning" v-if="">查無資料，請檢查篩選條件</v-alert>
 
-        <v-data-table
-          :headers="headers"
-          :items="originData"
-          :items-per-page="5"
-          class="elevation-1"
-        >
-          <template v-slot:item.tags="{ item }">
-            <v-item-group multiple>
-              <v-item v-for="tag in item.tags" :key="tag">
-                <v-chip @click="tagClick(tag)">
-                  {{ tag }}
-                </v-chip>
-              </v-item>
-            </v-item-group>
-          </template>
-          <template v-slot:item.website="{ item }">
-            <a :href="item.website" target="_blank" rel="nofollow" class="text-decoration-none">
-              <v-icon>mdi-open-in-new</v-icon>
-            </a>
-          </template>
-        </v-data-table>
+          <v-data-table
+            :headers="headers"
+            :items="originData"
+            :items-per-page="5"
+            item-key="name"
+            class="elevation-1"
+            :search="search"
+            :expanded.sync="expanded"
+            show-expand
+          >
+            <template v-slot:expanded-item="{ headers, item }">
+              <td :colspan="headers.length">
+                {{ item.comment }}
+                <span class="font-weight-bold" v-if="!item.comment">目前沒有詳細資料</span>
+              </td>
+            </template>
+            <template v-slot:no-results>
+              <v-alert type="warning" class="my-2">目前沒有符合的資料</v-alert>
+            </template>
+            <template v-slot:item.tags="{ item }">
+              <v-item-group multiple>
+                <v-item v-for="tag in item.tags" :key="tag">
+                  <v-chip @click="tagClick(tag)">
+                    {{ tag }}
+                  </v-chip>
+                </v-item>
+              </v-item-group>
+            </template>
+            <template v-slot:item.website="{ item }">
+              <a :href="item.website" target="_blank" rel="nofollow" class="text-decoration-none">
+                <v-icon>mdi-open-in-new</v-icon>
+              </a>
+            </template>
+          </v-data-table>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -140,23 +70,20 @@
 <script>
 import originData from '../assets/camp_data/111summer.json'
 
-const pageSelect = [
-  5, 10, 20, 50, 100
-]
 export default {
   name: 'IndexPage',
   data() {
     return {
+      expanded: [],
       originData,
-      searchContent: '',
+      search: '',
       tags: [],
       categories: [...new Set(originData.flatMap(r => r.tags))],
-      dialog: {},
-      headers: [
-        /*{
-          text: '#',
-          value: ''
-        },*/
+    }
+  },
+  computed: {
+    headers() {
+      return [
         {
           text: '名稱',
           value: 'name'
@@ -191,7 +118,8 @@ export default {
         },
         {
           text: '標籤',
-          value: 'tags'
+          value: 'tags',
+          filter: this.tagsFilter
         }
       ]
     }
@@ -200,19 +128,14 @@ export default {
     tagClick(tag) {
       if (!this.tags.includes(tag)) {
         this.tags.push(tag)
-        this.page = 1
-        this.resetPage()
       }
+    },
+    tagsFilter(value) {
+      if (!(this.tags) || this.tags.length === 0) {
+        return true;
+      }
+      return value.some(t => this.tags.includes(t)) || this.tags.length === 0
     }
-  },
-  watch: {
-    searchContent() {
-      this.page = 1
-      this.resetPage()
-    }
-  },
-  mounted() {
-
   }
 }
 </script>
