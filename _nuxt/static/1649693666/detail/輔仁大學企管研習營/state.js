d58d205ff8c03591c9fd4f1d0e814b17772913de
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-11 16:18:10 +0000

    deploy: 59d6cb249547b960cdb03688c3589d2286edfa4d

--- _nuxt/static/1649693666/detail/輔仁大學企管研習營/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649693666",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E8%BC%94%E4%BB%81%E5%A4%A7%E5%AD%B8%E4%BC%81%E7%AE%A1%E7%A0%94%E7%BF%92%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1649693870/detail/東海大學企管營/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649693870",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E6%9D%B1%E6%B5%B7%E5%A4%A7%E5%AD%B8%E4%BC%81%E7%AE%A1%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
