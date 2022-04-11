aaaf6ee290056ee2d7f8a555a18e2e0036999e21
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-11 16:09:39 +0000

    deploy: 59d6cb249547b960cdb03688c3589d2286edfa4d

--- _nuxt/static/1649693353/detail/陽明交通大學醫學營/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649693353",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E9%99%BD%E6%98%8E%E4%BA%A4%E9%80%9A%E5%A4%A7%E5%AD%B8%E9%86%AB%E5%AD%B8%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
