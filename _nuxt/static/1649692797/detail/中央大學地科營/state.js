9cd0a120ef204fe65585406daeb4939876a8016f
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-11 16:00:24 +0000

    deploy: 59d6cb249547b960cdb03688c3589d2286edfa4d

---
+++ _nuxt/static/1649692797/detail/中央大學地科營/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649692797",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E4%B8%AD%E5%A4%AE%E5%A4%A7%E5%AD%B8%E5%9C%B0%E7%A7%91%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
