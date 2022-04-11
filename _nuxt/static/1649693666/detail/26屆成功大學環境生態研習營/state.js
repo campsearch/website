8e5657624d1ae7267a6dad0c8747faac9ad3c75a
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-11 16:14:48 +0000

    deploy: 59d6cb249547b960cdb03688c3589d2286edfa4d

---
+++ _nuxt/static/1649693666/detail/26屆成功大學環境生態研習營/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649693666",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F26%E5%B1%86%E6%88%90%E5%8A%9F%E5%A4%A7%E5%AD%B8%E7%92%B0%E5%A2%83%E7%94%9F%E6%85%8B%E7%A0%94%E7%BF%92%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
