26227f7477f6200fc2f0504a4b09e9742d3d86c9
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-11 16:31:20 +0000

    deploy: 59d6cb249547b960cdb03688c3589d2286edfa4d

---
+++ _nuxt/static/1649694651/detail/政治大學歐洲語言與文化營/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649694651",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E6%94%BF%E6%B2%BB%E5%A4%A7%E5%AD%B8%E6%AD%90%E6%B4%B2%E8%AA%9E%E8%A8%80%E8%88%87%E6%96%87%E5%8C%96%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
