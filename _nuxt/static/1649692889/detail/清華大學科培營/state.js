b59cecc576fa8d75d40e391a03fdcaa2816b85f7
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-11 16:02:02 +0000

    deploy: 59d6cb249547b960cdb03688c3589d2286edfa4d

--- _nuxt/static/1649692889/detail/清華大學科培營/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649692889",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E6%B8%85%E8%8F%AF%E5%A4%A7%E5%AD%B8%E7%A7%91%E5%9F%B9%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- _nuxt/static/1649692889/detail/第八屆臺師大科技營/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649692889",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E7%AC%AC%E5%85%AB%E5%B1%86%E8%87%BA%E5%B8%AB%E5%A4%A7%E7%A7%91%E6%8A%80%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
