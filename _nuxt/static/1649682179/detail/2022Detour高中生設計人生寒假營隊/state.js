1451a89f253c4f34a3fb7b43729ead2fbff46f9b
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-11 13:03:25 +0000

    deploy: 59d6cb249547b960cdb03688c3589d2286edfa4d

---
+++ _nuxt/static/1649682179/detail/2022Detour高中生設計人生寒假營隊/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649682179",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F2022Detour%E9%AB%98%E4%B8%AD%E7%94%9F%E8%A8%AD%E8%A8%88%E4%BA%BA%E7%94%9F%E5%AF%92%E5%81%87%E7%87%9F%E9%9A%8A",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
