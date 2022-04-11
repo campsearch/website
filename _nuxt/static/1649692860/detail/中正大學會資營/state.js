1b2e9b482a3d5d04a963d7482370b975defc09d5
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-11 16:01:34 +0000

    deploy: 59d6cb249547b960cdb03688c3589d2286edfa4d

--- _nuxt/static/1649692860/detail/中正大學會資營/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649692860",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E4%B8%AD%E6%AD%A3%E5%A4%A7%E5%AD%B8%E6%9C%83%E8%B3%87%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
