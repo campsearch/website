f33a9ee0c93991a7936f688eaa730697ee5b9759
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-11 16:01:52 +0000

    deploy: 59d6cb249547b960cdb03688c3589d2286edfa4d

---
+++ _nuxt/static/1649692889/detail/成功大學心理科普營/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649692889",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E6%88%90%E5%8A%9F%E5%A4%A7%E5%AD%B8%E5%BF%83%E7%90%86%E7%A7%91%E6%99%AE%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
