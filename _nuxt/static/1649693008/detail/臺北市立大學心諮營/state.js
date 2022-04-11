3e7debd22939f6063724a5398c5883ea42e693fd
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-11 16:03:50 +0000

    deploy: 59d6cb249547b960cdb03688c3589d2286edfa4d

---
+++ _nuxt/static/1649693008/detail/臺北市立大學心諮營/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649693008",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E8%87%BA%E5%8C%97%E5%B8%82%E7%AB%8B%E5%A4%A7%E5%AD%B8%E5%BF%83%E8%AB%AE%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
