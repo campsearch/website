309ecb758e00ff8e8f68cc49cada1b447e7ec6ae
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-11 16:18:24 +0000

    deploy: 59d6cb249547b960cdb03688c3589d2286edfa4d

--- _nuxt/static/1649693877/detail/東海大學會計營/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649693877",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E6%9D%B1%E6%B5%B7%E5%A4%A7%E5%AD%B8%E6%9C%83%E8%A8%88%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
