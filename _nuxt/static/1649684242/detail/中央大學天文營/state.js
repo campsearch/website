a456409d17f939a505d43aaee047f86df2152739
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-11 13:37:49 +0000

    deploy: 59d6cb249547b960cdb03688c3589d2286edfa4d

--- _nuxt/static/1649684242/detail/中央大學天文營/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649684242",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E4%B8%AD%E5%A4%AE%E5%A4%A7%E5%AD%B8%E5%A4%A9%E6%96%87%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
