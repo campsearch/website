de0b69bcea55d08a6a08e5848e674db3e58f97ad
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-12 03:50:20 +0000

    deploy: ab3cfc72d8eebdaa79f04fef247dbcf300849a61

---
+++ _nuxt/static/1649735415/detail/2022成功大學化學營/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649735415",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F2022%E6%88%90%E5%8A%9F%E5%A4%A7%E5%AD%B8%E5%8C%96%E5%AD%B8%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
