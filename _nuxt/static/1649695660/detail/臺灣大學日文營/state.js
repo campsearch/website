8bd5e1d1bba5ac37af0a173a4c8e545ed996b1a7
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-12 02:46:08 +0000

    deploy: 88f4dbc8667d1fe21c8ea74a3b35edd2d342d4b3

--- _nuxt/static/1649695660/detail/臺灣大學日文營/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649695660",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E8%87%BA%E7%81%A3%E5%A4%A7%E5%AD%B8%E6%97%A5%E6%96%87%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
