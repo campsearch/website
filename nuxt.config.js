6d04c198df3e6f2221620331da7565a8d3e74549
Your Name <you@example.com>
2022-04-12 12:59:48 +0800

    add option for github page

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,6 +57,10 @@ export default {
         '@fengsi/nuxt-axios-cache'
     ],
 
+    generate: {
+        fallback: "404.html"
+    },
+
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
         // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
