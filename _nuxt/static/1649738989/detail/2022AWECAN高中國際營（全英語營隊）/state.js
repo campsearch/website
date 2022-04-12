36fd447319b377121e9c0f25e168f5b9ac34633e
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-12 04:50:02 +0000

    deploy: 6637173fe43563dc53518f43aed2d33677591c7a

---
+++ _nuxt/static/1649738989/detail/2022AWECAN高中國際營（全英語營隊）/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649738989",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F2022AWECAN%E9%AB%98%E4%B8%AD%E5%9C%8B%E9%9A%9B%E7%87%9F%EF%BC%88%E5%85%A8%E8%8B%B1%E8%AA%9E%E7%87%9F%E9%9A%8A%EF%BC%89",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
