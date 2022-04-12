d3d3d5fa0b5c5848df7f30a09cbd53a94414e943
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-12 03:48:08 +0000

    deploy: ab3cfc72d8eebdaa79f04fef247dbcf300849a61

---
+++ _nuxt/static/1649735283/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fdetail\u002Flist","\u002Fdetail\u002F2022第14屆陽明交通大學人文社會科學營《下一站，客棧》","\u002Fdetail\u002F2022輔大社會營《第一人稱》","\u002Fdetail\u002F成功大學材料營","\u002Fdetail\u002F東華大學-觀遊營","\u002Fdetail\u002F北科資財營－先行者Theforerunner","\u002Fdetail\u002F雙橡｜Amazon企業策略營","\u002Fdetail\u002F佛光大學心理營隊"]})
