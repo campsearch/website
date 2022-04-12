0a632085a3074351424e44bbc76d18383b8527e0
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-12 14:12:02 +0000

    deploy: db5a38972d19e0befd644cf1d1a6be52d7dc59d7

---
+++ _nuxt/static/1649772701/detail/東華大學-觀遊營/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649772701",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E6%9D%B1%E8%8F%AF%E5%A4%A7%E5%AD%B8-%E8%A7%80%E9%81%8A%E7%87%9F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
