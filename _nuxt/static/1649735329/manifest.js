bbb7235510b71a35d1d7cc7e1252eb3ead53d45f
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-12 03:49:09 +0000

    deploy: ab3cfc72d8eebdaa79f04fef247dbcf300849a61

--- _nuxt/static/1649735329/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fdetail\u002Flist","\u002Fdetail\u002F北科資財營－先行者Theforerunner","\u002Fdetail\u002F輔仁大學醫學營","\u002Fdetail\u002F雲科大創意生活設計營∥盜創奇航","\u002Fdetail\u002F2022AWECAN高中國際營（全英語營隊）","\u002Fdetail\u002F世新大學2022第七屆公關營《PlayeRs脫罪玩家》"]})
