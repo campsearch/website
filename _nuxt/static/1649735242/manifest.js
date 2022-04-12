92269aba5862dd8ef21f45f6867e095560f52fc4
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-04-12 03:47:28 +0000

    deploy: ab3cfc72d8eebdaa79f04fef247dbcf300849a61

---
+++ _nuxt/static/1649735242/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fdetail\u002Flist","\u002Fdetail\u002F國立彰化師範大學輔導營","\u002Fdetail\u002F中央大學企管營","\u002Fdetail\u002F成功大學國家領導研習營","\u002Fdetail\u002F臺灣大學日文營","\u002Fdetail\u002F東華大學-觀遊營","\u002Fdetail\u002F東海大學企管營"]})
