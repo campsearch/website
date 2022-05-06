e07f8b6e1ed4b9b99bc1f87add2db9ca33cdd83d
hsuan1117 <hsuan1117@users.noreply.github.com>
2022-05-06 04:57:35 +0000

    deploy: 4c958c252baec4d2848d76acbe7178e657b7be0b

---
+++ _nuxt/static/1651813036/detail/暑期生科營-廂房的神祕客/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651813036",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdetail\u002F%E6%9A%91%E6%9C%9F%E7%94%9F%E7%A7%91%E7%87%9F-%E5%BB%82%E6%88%BF%E7%9A%84%E7%A5%9E%E7%A5%95%E5%AE%A2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
